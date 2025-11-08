ae7824c45be2e18f32280e0a47f8546d17ea6177
myrirado24 <gui18002@byui.edu>
2025-11-07 20:00:58 -0700

    Completed the first step where user created the magic number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,27 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        Console.WriteLine("What is the magic number?");
+        string magicNumber = Console.ReadLine();
+        int magicNumber2 = int.Parse(magicNumber);
+
+
+        Console.WriteLine("What is the magic number?");
+        string guess = Console.ReadLine();
+        int guess2 = int.Parse(guess);
+
+        if (guess2 > magicNumber2)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (guess2 < magicNumber2)
+        {
+            Console.WriteLine("Higher");
+        }
+        else
+        {
+            Console.WriteLine("Correct");
+        }
     }
 }
