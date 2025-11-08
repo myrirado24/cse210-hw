6c516a516362503743f0733da0a6e95a9f390abe
myrirado24 <gui18002@byui.edu>
2025-11-07 19:19:54 -0700

    Added the if stated of the grades

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -9,10 +9,26 @@ class Program
         Console.Write("What is your grade?");
         string grade = Console.ReadLine();
         int persent = int.Parse(grade);
-    
+
         if (persent >= 90)
         {
             Console.WriteLine("A");
+        }
+        else if (persent >= 80)
+        {
+            Console.WriteLine("B");
+        }
+        else if (persent >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if (persent >= 80)
+        {
+            Console.WriteLine("D");
+        }
+        else
+        {
+            Console.WriteLine("F");
         }
     
 
