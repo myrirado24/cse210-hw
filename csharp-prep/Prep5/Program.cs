09f273167e08156a4bd08d089914ef66b235865d
myrirado24 <gui18002@byui.edu>
2025-11-08 00:24:44 -0700

    Added the user input questions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+
+        DisplayWelcomeMessage();
+
+        string userName = PromptUserName;
+        int userNumber = PromptUserNumber;
+        int squaredNumber = SquareNumber(userNumber);
+        int birthYear;
+        PromtUserBirthYear(out birthYear);
     }
 }
