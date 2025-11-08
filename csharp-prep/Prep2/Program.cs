9ca9fbdd4c27eefc9e1868626628f6755176e44f
myrirado24 <gui18002@byui.edu>
2025-11-07 19:16:41 -0700

    added the varible and the convertion to a persentage

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade?");
+        string grade = Console.ReadLine();
+        int persent = int.Parse(grade);
+    
+        if (persent >= 90)
+        {
+            Console.WriteLine("A");
+        }
+    
+
     }
 }
