324630ef705bc06896c43e0a0d8bb10bdc29897e
myrirado24 <gui18002@byui.edu>
2025-11-07 20:25:16 -0700

    Added the ramdon generated number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -10,18 +10,21 @@ class Program
         //string magicNumber = Console.ReadLine();
         //int magicNumber2 = int.Parse(magicNumber);
 
+        Random random = new Random();
+        int magicNumber = random.Next(1, 100);
+
         int guess = -1;
 
-        while (guess != magicNumber2)
+        while (guess != magicNumber)
         {
             Console.WriteLine("What is your guess?");
             guess = int.Parse(Console.ReadLine());
 
-            if (guess > magicNumber2)
+            if (guess > magicNumber)
             {
                 Console.WriteLine("Lower");
             }
-            else if (guess < magicNumber2)
+            else if (guess < magicNumber)
             {
                 Console.WriteLine("Higher");
             }
