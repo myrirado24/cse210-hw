d3a98e21614ff6045cb4e51b18eb499e80cf1001
myrirado24 <gui18002@byui.edu>
2025-11-07 23:40:56 -0700

    Added the number varible on the loop

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while ()
+        {
+            Console.WriteLine("Enter number: ");
+            string number = Console.ReadLine();
+
+            if ()
+        }
     }
 }
