223385fe2f7f0e319f6ef9453c3e3bf94578eeee
myrirado24 <gui18002@byui.edu>
2025-11-12 19:35:08 -0700

    added a list on the resume class and fix the display public void on job class

--- prepare/Learning02/job.cs
+++ prepare/Learning02/job.cs
@@ -2,7 +2,7 @@
 //Keeps track of the company, job title, start year, and end year.
 //Behaviors:
 //Displays the job information in the format "Job Title (Company) StartYear-EndYear", for example: "Software Engineer (Microsoft) 2019-2022".
-
+using System;
 public class Job
 {
     public string _company = "";
--- prepare/Learning02/resume.cs
+++ prepare/Learning02/resume.cs
@@ -2,3 +2,10 @@
 //Keeps track of the person's name and a list of their jobs.
 //Behaviors:
 //Displays the resume, which shows the name first, followed by displaying each one of the jobs.
+using System;
+public class Resume
+{
+    public List<Job> _jobs = new List<Job>();
+
+    
+}
