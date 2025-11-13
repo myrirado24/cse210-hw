d65d24dccf8b2568457a58c5d6696a83093cd44e
myrirado24 <gui18002@byui.edu>
2025-11-12 19:46:50 -0700

    Added display method on Resume class

--- prepare/Learning02/resume.cs
+++ prepare/Learning02/resume.cs
@@ -3,10 +3,21 @@
 //Behaviors:
 //Displays the resume, which shows the name first, followed by displaying each one of the jobs.
 using System;
+using System.Reflection.Emit;
 public class Resume
 {
     public string _name;
     public List<Job> _jobs = new List<Job>();
 
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        foreach (Job job in _jobs)
+        {
+            job.DisplayJobDetails();
+        }
+    }
 
 }
