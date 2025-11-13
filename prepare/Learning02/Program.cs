3f1171dd5f755d75d31908baad3895fcce9f0b0d
myrirado24 <gui18002@byui.edu>
2025-11-12 19:13:58 -0700

    Added not notation for job 1 and 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -6,6 +6,12 @@ class Program
     {
         Console.WriteLine("Hello Learning02 World!");
 
-        
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+
+        Job job2 = new Job();
+        job2._jobTitle = "";
+        job2._company = "Apple";
     }
 }
--- prepare/Learning02/job.cs
+++ prepare/Learning02/job.cs
@@ -7,6 +7,6 @@ public class Job
 {
     public string _company = "";
     public string _jobTitle = "";
-    public int _startYear = "";
-    public int _endYear = "";
+    public int _startYear = 0;
+    public int _endYear = 0;
 }
