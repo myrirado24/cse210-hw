5e87e3c1ca391e9cc62b3eaa2896c395b75cead7
myrirado24 <gui18002@byui.edu>
2025-11-12 19:21:41 -0700

    Added te member function to display the job details

--- prepare/Learning02/job.cs
+++ prepare/Learning02/job.cs
@@ -7,6 +7,12 @@ public class Job
 {
     public string _company = "";
     public string _jobTitle = "";
-    public int _startYear = 0;
-    public int _endYear = 0;
+    public string _startYear = "";
+    public string _endYear = "";
+    
+    public void DisplayJobDetails()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
 }
+
