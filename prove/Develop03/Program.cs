49389df48c0e8286981110c3776bd5b32a1d2f07
myrirado24 <gui18002@byui.edu>
2025-11-22 19:07:04 -0700

    Added the scripture and te reference to the program

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,10 +1,15 @@
 using System;
-
+using System.Collections.Generic;
+using System.IO;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop03 World!");
+
+        Scripture s = new Scrpsture("Be ye mindful always of his covenant; the word which he commanded to a thousand generations;");
+        Reference r = new Reference("1 Chornicles", 16, 15);
+        bool finished = false;
     }
 }
