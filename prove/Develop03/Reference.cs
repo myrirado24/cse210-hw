f35acad160f128a505724494e30436f4c9023384
myrirado24 <gui18002@byui.edu>
2025-11-22 19:31:52 -0700

    realised I had my code for refence on sricture so I moved it

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -1,4 +1,23 @@
 public class Reference
 {
-    
+    private string _book = "";
+    private int _charpter;
+    private int _firstVerse;
+    private int _lastVerse;
+
+    public Reference()
+    {
+        _book = "";
+        _charpter = 0;
+        _firstVerse = 0;
+        _lastVerse = 0;
+    }
+
+    public Reference(string book, int chapter, int _firstVerse)
+    {
+        _book = book;
+        _charpter = chapter;
+        _firstVerse = firstVerse;
+        _lastVerse = 0;
+    }
 }
